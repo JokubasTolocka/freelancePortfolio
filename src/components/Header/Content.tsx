dde04d62236fc164ef23eda5b577dc05d0ff470c
Jokūbas Toločka <jokubas.tj@gmail.com>
2024-10-08 19:14:55 +0300

    temporary solution to header animations, saved position is using last saved position

--- src/components/Header/Content.tsx
+++ src/components/Header/Content.tsx
@@ -4,6 +4,16 @@ import Typography from "../typography/HeadingTypography";
 import Squiggle from "../Squiggle";
 import SocialMedia from "../SocialMedia";
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 72px;
+  padding: 0 32px;
+  width: 100%;
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.black}`};
+`;
+
 const RightContainer = styled.div`
   display: flex;
   gap: 24px;
@@ -11,14 +21,14 @@ const RightContainer = styled.div`
 `;
 
 const Content = () => (
-  <>
+  <Wrapper>
     <Typography variant="Heading4">Let's do it.</Typography>
     <RightContainer>
       <Typography variant="Heading4">jokubas.tj@gmail.com</Typography>
       <Squiggle />
       <SocialMedia />
     </RightContainer>
-  </>
+  </Wrapper>
 );
 
 export default Content;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import useMousePos from "../../hooks/useMousePos";
 import Content from "./Content";
 
 const Wrapper = styled.div`
-  padding: 0 32px;
-  height: 72px;
   width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: ${({ theme }) => `1px solid ${theme.colors.black}`};
+  position: relative;
+  z-index: 2;
 `;
 
 const MaskWrapper = styled(Wrapper)`
   position: absolute;
+  z-index: 3;
   top: 0;
   background-color: ${({ theme }) => theme.colors.black};
   color: ${({ theme }) => theme.colors.white};
-  mask: url("./cursorMask.svg");
+  -webkit-mask: url("./cursorMask.svg");
   mask-repeat: no-repeat;
 
   svg {
@@ -28,35 +25,84 @@ const MaskWrapper = styled(Wrapper)`
   }
 `;
 
+const ClipContent = styled.div`
+  overflow: hidden;
+  position: relative;
+`;
+
+const FULL_CIRCLE_SIZE = 2000;
+
+const CircleBase = styled.div`
+  border-radius: ${FULL_CIRCLE_SIZE}px;
+  position: absolute;
+`;
+
+const CircleYellow = styled(CircleBase)`
+  background-color: ${({ theme }) => theme.colors.complimentary.yellow};
+  zindex: 1;
+`;
+
+const CircleGreen = styled(CircleBase)`
+  background-color: ${({ theme }) => theme.colors.complimentary.green};
+  zindex: 2;
+`;
+
+const CircleBlue = styled(CircleBase)`
+  background-color: ${({ theme }) => theme.colors.complimentary.blue};
+  zindex: 3;
+`;
+
 const Header = () => {
   const { x, y } = useMousePos();
   const [isMouseHovering, setIsMouseHovering] = useState(false);
+  const [{ x: savedX, y: savedY }, setSavedFirstPosition] = useState({
+    x: 0,
+    y: 0,
+  });
 
-  const CIRCLE_SIZE = isMouseHovering ? 1000 : 0;
+  const CIRCLE_SIZE = isMouseHovering ? FULL_CIRCLE_SIZE : 0;
 
-  const onMouseEnter = () => setIsMouseHovering(true);
-  const onMouseLeave = () => setIsMouseHovering(false);
+  const onMouseEnter = () => {
+    setSavedFirstPosition({ x, y });
+    setIsMouseHovering(true);
+  };
+  const onMouseLeave = () => {
+    setSavedFirstPosition({ x, y });
+    setIsMouseHovering(false);
+  };
+
+  console.log({ x, y, savedX, savedY });
 
   return (
-    <>
+    <ClipContent>
       <Wrapper>
         <Content />
       </Wrapper>
+      <CircleBlue
+        as={motion.div}
+        animate={{
+          top: savedY - CIRCLE_SIZE / 2,
+          left: savedX - CIRCLE_SIZE / 2,
+          height: CIRCLE_SIZE,
+          width: CIRCLE_SIZE,
+        }}
+        transition={{ type: "tween", ease: "easeOut", duration: 0.3 }}
+      />
       <MaskWrapper
         as={motion.div}
         animate={{
           WebkitMaskSize: `${CIRCLE_SIZE}px`,
-          WebkitMaskPosition: `${x - CIRCLE_SIZE / 2}px ${
-            y - CIRCLE_SIZE / 2
+          WebkitMaskPosition: `${savedX - CIRCLE_SIZE / 2}px ${
+            savedY - CIRCLE_SIZE / 2
           }px`,
         }}
-        transition={{ type: "tween", ease: "backOut" }}
+        transition={{ type: "tween", ease: "easeIn", delay: 0.2 }}
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
         <Content />
       </MaskWrapper>
-    </>
+    </ClipContent>
   );
 };
 
