66a3f6562a082d67d8483da08ea92c8c0f158c0d
JokubasTolocka <jokubas.tj@gmail.com>
2025-01-21 19:41:41 +0200

    adjust footer for global layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ import CopyOverlayContainer from "./CopyOverlayContainer";
 import constants from "../constants/constants.json";
 import LinkedInIcon from "../assets/icons/linkedIn.svg";
 import GithubIcon from "../assets/icons/github.svg";
+import { MAX_SITE_WIDTH } from "../utils/globalStyles";
 
 const FOOTER_ITEMS = [
   { title: "Work" },
@@ -15,49 +16,51 @@ const FOOTER_ITEMS = [
 
 const Footer = () => (
   <Wrapper>
-    <TopContent>
-      <PersonalInfoWrapper>
-        <PersonalInfoNameWrapper>
-          <Typography variant={Heading.H4}>Jokūbas Toločka</Typography>
-          <SocialMediaWrapper>
-            <LinkText
-              href={constants.LINKED_IN_URL}
-              target="_blank"
-              rel="noopener"
-            >
-              <StyledLinkedInIcon />
-            </LinkText>
-            <LinkText
-              href={constants.GITHUB_URL}
-              target="_blank"
-              rel="noopener"
-            >
-              <StyledGithubIcon />
-            </LinkText>
-          </SocialMediaWrapper>
-        </PersonalInfoNameWrapper>
+    <Content>
+      <TopContent>
+        <PersonalInfoWrapper>
+          <PersonalInfoNameWrapper>
+            <Typography variant={Heading.H4}>Jokūbas Toločka</Typography>
+            <SocialMediaWrapper>
+              <LinkText
+                href={constants.LINKED_IN_URL}
+                target="_blank"
+                rel="noopener"
+              >
+                <StyledLinkedInIcon />
+              </LinkText>
+              <LinkText
+                href={constants.GITHUB_URL}
+                target="_blank"
+                rel="noopener"
+              >
+                <StyledGithubIcon />
+              </LinkText>
+            </SocialMediaWrapper>
+          </PersonalInfoNameWrapper>
+          <Typography variant={Heading.H5}>
+            Freelance Designer / Frontend Developer
+          </Typography>
+        </PersonalInfoWrapper>
+        <CopyOverlayContainer>
+          <PointerTypography variant={Heading.H1}>
+            {constants.EMAIL}
+          </PointerTypography>
+        </CopyOverlayContainer>
+      </TopContent>
+      <BottomContent>
         <Typography variant={Heading.H5}>
-          Freelance Designer / Frontend Developer
+          Website Designed + Developed by Me
         </Typography>
-      </PersonalInfoWrapper>
-      <CopyOverlayContainer>
-        <PointerTypography variant={Heading.H1}>
-          {constants.EMAIL}
-        </PointerTypography>
-      </CopyOverlayContainer>
-    </TopContent>
-    <BottomContent>
-      <Typography variant={Heading.H5}>
-        Website Designed + Developed by Me
-      </Typography>
-      <Typography variant={Heading.H5}>
-        <BottomLinks>
-          {FOOTER_ITEMS.map(({ title }) => (
-            <LinkText>{title}</LinkText>
-          ))}
-        </BottomLinks>
-      </Typography>
-    </BottomContent>
+        <Typography variant={Heading.H5}>
+          <BottomLinks>
+            {FOOTER_ITEMS.map(({ title }) => (
+              <LinkText>{title}</LinkText>
+            ))}
+          </BottomLinks>
+        </Typography>
+      </BottomContent>
+    </Content>
   </Wrapper>
 );
 
@@ -65,12 +68,19 @@ export default Footer;
 
 const Wrapper = styled.div`
   width: 100%;
-  padding: 40px;
   background-color: ${({ theme }) => theme.colors.white};
   color: ${({ theme }) => theme.colors.black.dark};
   display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  max-width: ${MAX_SITE_WIDTH}px;
+  width: 100%;
+  display: flex;
   flex-direction: column;
   gap: 40px;
+  padding: 40px;
 `;
 
 const TopContent = styled.div`
