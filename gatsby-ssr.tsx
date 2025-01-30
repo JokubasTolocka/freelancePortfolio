1bb442278925d917a568a3b26e5dc692a11d5d88
JokubasTolocka <jokubas.tj@gmail.com>
2025-01-30 20:41:31 +0200

    update setHeadComponents

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -4,25 +4,31 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./src/utils/globalStyles";
 import GlobalFonts from "./src/utils/globalFonts";
 
+const HeadComponents = [
+  <link
+    rel="preload"
+    href="/fonts/ppneuemontreal-medium.otf"
+    as="font"
+    type="font/otf"
+    crossOrigin="anonymous"
+    key="neueMontrealFont-medium"
+  />,
+  <link
+    rel="preload"
+    href="/fonts/ppneuemontreal-book.otf"
+    as="font"
+    type="font/otf"
+    crossOrigin="anonymous"
+    key="neueMontrealFont-book"
+  />,
+];
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents(HeadComponents);
+};
+
 export const wrapRootElement = ({ element, setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel="preload"
-      href="/fonts/ppneuemontreal-medium.otf"
-      as="font"
-      type="font/otf"
-      crossOrigin="anonymous"
-      key="neueMontrealFont-medium"
-    />,
-    <link
-      rel="preload"
-      href="/fonts/ppneuemontreal-book.otf"
-      as="font"
-      type="font/otf"
-      crossOrigin="anonymous"
-      key="neueMontrealFont-book"
-    />,
-  ]);
+  setHeadComponents([]);
 
   return (
     <ThemeProvider theme={theme}>
